b1d463d1d1d97a37d95eccabe18e1ea0a895ccbf
khaled Reza <khaledreza@gmail.com>
2022-09-21 15:21:56 +0200

    Single product page was developed

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,3 +1,5 @@
+import { Add, Remove } from "@material-ui/icons";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
@@ -20,15 +22,73 @@ const Image = styled.img`
 
 const InfoContainer = styled.div`
   flex: 1;
+  height: 90vh;
 `;
-const Description = styled.div`
-  height: 100%;
-  background-color: brown;
+
+const Title = styled.h1``;
+
+const Description = styled.span``;
+
+const Price = styled.h3``;
+const FilterContainer = styled.div`
+  display: flex;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 50px;
+`;
+const FilterTitle = styled.div``;
+const FilterColor = styled.div`
+  margin-left: 5px;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 5px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+  margin: 30px;
+  display: flex;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Amount = styled.div`
+  height: 20px;
+  width: 30px;
+  border-radius: 30%;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  margin-left: 30px;
+  padding: 10px;
+  background-color: white;
+  cursor: pointer;
 `;
 
 const Product = () => {
+  const [quantity, setQuantity] = useState(0);
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
   const location = useLocation();
   const img = location.pathname.split("/")[2];
+
+  const adjustQuantity = (operation) => {
+    operation === "remove" && quantity > 1 && setQuantity(quantity - 1);
+    operation === "add" && setQuantity(quantity + 1);
+  };
   return (
     <Container>
       <Announcement />
@@ -38,7 +98,43 @@ const Product = () => {
           <Image src={popularProducts[2].img}></Image>
         </ImageContainer>
         <InfoContainer>
-          <Description></Description>
+          <Title>Colorful Shirt</Title>
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Description>
+          <Price>$50</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color:</FilterTitle>
+              <FilterColor
+                color="red"
+                onClick={(e) => setColor("red")}
+              ></FilterColor>
+              <FilterColor
+                color="blue"
+                onClick={(e) => setColor("blue")}
+              ></FilterColor>
+            </Filter>
+            <Filter>
+              <FilterTitle>Size:</FilterTitle>
+              <FilterSize onChange={(e) => setSize(e.target.value)}>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove onClick={() => adjustQuantity("remove")}>-</Remove>
+              <Amount>{quantity}</Amount>
+              <Add onClick={() => adjustQuantity("add")}>+</Add>
+            </AmountContainer>
+            <Button>Add to cart</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
     </Container>
